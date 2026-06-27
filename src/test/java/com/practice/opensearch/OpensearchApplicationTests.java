1d82e2a0929efa295314740a9704dbecba32cc12
Justin Joco <justin.joco@gmail.com>
2026-06-26 23:56:12 -0700

    first commit

---
+++ src/test/java/com/practice/opensearch/OpensearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.opensearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
